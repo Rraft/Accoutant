159652a700599a1e3a674d8220e810d4a87ed6cb
abc <1329938805@qq.com>
2025-08-11 20:21:06 +0800

    add:增加月度结算查看功能

--- src/components/views/ResultPage.vue
+++ src/components/views/ResultPage.vue
@@ -1,13 +1,81 @@
 <script>
 export default {
-  name: "ResultPage"
+  name: "ResultPage",
+
+  data(){
+    return{
+      result: [],
+    }
+  },
+
+  methods: {
+    /**
+     * @description: 获取月度结算
+     * @author 13299
+     */
+    getMonthResult(){
+      this.$axios({
+        url: this.$baseUrl + "/api/getMonthResult",
+        method: "GET"
+      }).then(res => {
+        if (res.data.code === 0){
+          this.result = res.data.data;
+        }
+      });
+    }
+  },
+
+  created() {
+    this.getMonthResult();
+  }
 }
 </script>
 
 <template>
-
+  <div class="center-box">
+    <div class="card">
+      <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+          <th scope="col">年份</th>
+          <th scope="col">月份</th>
+          <th scope="col">沈艺童's</th>
+          <th scope="col">徐涵浩's</th>
+          <th scope="col">吴锋's</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="item in result" :key="item.id">
+          <td>{{item.year}}</td>
+          <td>{{item.month}}</td>
+          <td>{{item.shen}}</td>
+          <td>{{item.xu}}</td>
+          <td>{{item.wu}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 
+.center-box{
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  font-size: 15px;
+}
+
+.card{
+  width: 70%;
+  margin-right: 20px;
+  margin-left: 20px;
+  margin-top: 10px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+}
 </style>
